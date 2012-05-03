ac4a4c5d6124f9a42da537bf406d7d695e7c4590
pentestmonkey <pentestmonkey@c57415a8-16fd-11de-9433-ad2e3fad96c5>
2012-05-03 19:59:39 +0000

    Added script to install some dependencies on ubuntu.  Need to expand this.

---
+++ trunk/apt-get-install-some-dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+apt-get install postgresql
+apt-get install libcrypt-ssleay-perl
+apt-get install nmap john
+apt-get install ike-scan
+apt-get install arp-scan
+apt-get install postgresql
+apt-get install sslscan
+apt-get install libpcap-dev
+apt-get install libssl-dev
+apt-get install libdbd-pg-perl
+apt-get install libparallel-forkmanager-perl
+apt-get install libdigest-md4-perl
+apt-get install libxml-simple-perl
+apt-get install ldap-utils
+apt-get install libio-pty-perl
+apt-get install libproc-simple-perl
+apt-get install libexpect-perl
