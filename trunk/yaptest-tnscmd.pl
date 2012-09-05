48f9c6dba9ca349f30bcaf1e0fc1a7314e305b1d
pentestmonkey <pentestmonkey@c57415a8-16fd-11de-9433-ad2e3fad96c5>
2012-09-05 10:33:41 +0000

    tnscmd now gathers information more reliably from oracle services

--- trunk/yaptest-tnscmd.pl
+++ trunk/yaptest-tnscmd.pl
@@ -7,7 +7,7 @@ use File::Basename;
 
 my $script_name = basename($0);
 my $usage = "Usage: $script_name
-Runs tnscmd.pl with the ping, version, status and services option on 
+Runs tnscmd.pl with the ping, version, status and services option on
 port that nmap thinks have service_name 'oracle-tns'.
 
 NB: tnscmd.pl is required to be in the path.
@@ -17,12 +17,12 @@ die $usage if shift;
 my $y = yaptest->new();
 
 for my $option ("ping", "version", "services", "status") {
-	$y->run_test(
-		command => "tnscmd $option --indent -h ::IP:: -p ::PORT::",
-		filter => { port_info => [ "nmap_service_name = oracle-tns", "nmap_service_name = oracle" ], ssl => 0 },
-		parallel_processes => 10,
-		output_file => "tnscmd-$option-::IP::-::PORT::.out",
-		timeout => 10,
-		parser => 'yaptest-parse-tnscmd.pl'
-	);
+        $y->run_test(
+                command => "tnscmd -h ::IP:: -p ::PORT:: --rawcmd '(CONNECT_DATA=(COMMAND=$option))' --indent",
+                filter => { port_info => [ "nmap_service_name = oracle-tns", "nmap_service_name = oracle" ], ssl => 0 },
+                parallel_processes => 10,
+                output_file => "tnscmd-$option-::IP::-::PORT::.out",
+                timeout => 10,
+                parser => 'yaptest-parse-tnscmd.pl'
+        );
 }
